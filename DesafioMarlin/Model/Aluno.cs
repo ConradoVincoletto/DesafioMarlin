82cf31b4db9efccf825ca5c468ef00864b7a94b6
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-01 11:53:21 -0300

    feat: criação do projeto e classe aluno

---
+++ DesafioMarlin/Model/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesafioMarlin.Model
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Mínimo de 3 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo de preenchimento obrigatório")]
+        [RegularExpression(@"^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$", ErrorMessage = "CPF inválido")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "O email é inválido")]
+        public string Email { get; set; }
+    }
+}
