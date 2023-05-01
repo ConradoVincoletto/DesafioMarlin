44fa1b39457dfe7f06d24df6ecfd07fffb4353b6
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-01 11:55:08 -0300

    feat: Escola Contexto

---
+++ DesafioMarlin/Context/EscolaContexto.cs
@@ -0,0 +1,18 @@
+﻿using DesafioMarlin.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioMarlin.Context
+{
+    public class EscolaContexto : DbContext
+    {
+        public EscolaContexto()
+        {
+        }
+
+        public EscolaContexto(DbContextOptions<EscolaContexto> options) : base(options)
+        {
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+    {
+    }
+}
